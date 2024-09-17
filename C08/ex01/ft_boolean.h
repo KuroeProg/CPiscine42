990158af791a8a4c826f542c70d9a6647a3b4bf3
Cloe Fiachetti <cfiachet@c2r3p1.42perpignan.fr>
2024-09-17 17:10:06 +0200

    C08 updated

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: cfiachet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 12:38:06 by cfiachet          #+#    #+#             */
-/*   Updated: 2024/09/16 14:21:14 by cfiachet         ###   ########.fr       */
+/*   Updated: 2024/09/17 16:09:42 by cfiachet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <unistd.h>
 
-# define EVEN(nbr) nbr % 2 == 0
+# define EVEN(nbr) ((nbr) % 2 == 0)
 # define TRUE 1
 # define FALSE 0
 # define EVEN_MSG "I have an even number of arguments.\n"
--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,51 +6,76 @@
 /*   By: cfiachet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 16:21:18 by cfiachet          #+#    #+#             */
-/*   Updated: 2024/09/16 18:39:05 by cfiachet         ###   ########.fr       */
+/*   Updated: 2024/09/17 16:56:00 by cfiachet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-char *ft_strcpy(char *dest, char *src)
+
+#include "ft_stock_str.h"
+#include <stdlib.h>
+
+int	ft_strlen(char *str);
+
+char	*ft_strcpy(char *dest, char *src)
 {
 	int	i;
-	i = 0;
 
-	dest = malloc(sizeof(char *) * (ft_strlen(src)));
+	i = 0;
+	dest = malloc(sizeof(char) * (ft_strlen(src) + 1));
 	if (dest == NULL)
-		return NULL;
-	while(src[i])
+		return (dest);
+	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-
+	return (dest);
+}
 
 int	ft_strlen(char *str)
 {
 	int	i;
+
 	i = 0;
 	while (str[i])
 		i++;
 	return (i);
 }
 
-struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+void	ft_free(t_stock_str *tab, int i)
 {
-	int	i;
+	int	j;
+
+	j = 0;
+	while (j < i)
+	{
+		free (tab[j].copy);
+		j++;
+	}
+	free(tab);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int				i;
+	t_stock_str		*tab;
 
 	i = 0;
-	t_stock_str *tab;
-	tab = malloc(sizeof(t_stock_str) * ac);
-	if (tab == NULL)
-		return NULL;
-	while (str[i])
+	tab = malloc(sizeof(t_stock_str) * (ac + 1));
+	if (tab == 0)
+		return (tab);
+	while (i < ac)
 	{
 		tab[i].size = ft_strlen(av[i]);
 		tab[i].str = av[i];
-		tab[i].copy = ft_strcpy(tab[i], av[i]);
+		tab[i].copy = ft_strcpy(tab[i].str, av[i]);
+		if (tab[i].copy == NULL)
+		{
+			ft_free(tab, i);
+			return (NULL);
+		}
 		i++;
 	}
 	tab[i].str = 0;
-	
 	return (tab);
 }
